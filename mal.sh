3e496c14ec3919c75f4180b79493318f84045b4f
macubergeek <42jameskelly@gmail.com>
2016-04-28 09:49:49 -0400

    adding a few tools

--- mal.sh
+++ mal.sh
@@ -13,3 +13,4 @@ done
 #wget https://googledrive.com/host/0B6fULLT_NpxMQ1Rrb1drdW42SkE/remnux-6.0-ova-public.ova
 apt-get update && apt-get upgrade && \
 apt-get install git libssl-dev bison libjansson-dev
+apt-get install dh-autoreconf
