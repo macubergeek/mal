2d0e46a01fb185fde67510dff9195808ec8e334f
macubergeek <42jameskelly@gmail.com>
2016-04-28 19:16:39 -0400

    adding a few tools

--- yara-install.sh
+++ yara-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt/yara
+cd /home/cuckoo/yara
 apt-get install autoconf libtool libjansson-dev libmagic-dev libssl-dev -y
 sudo ./bootstrap.sh && ./configure --enable-cuckoo --enable-magic && \
 cd /home/cuckoo/yara/yara-python && \
