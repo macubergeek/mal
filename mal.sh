9ee3178d622f81786448c678367b1ca9428ee77d
macubergeek <42jameskelly@gmail.com>
2016-04-28 07:08:48 -0400

    adding a few tools

---
+++ mal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a=`cat <<-EOF
+https://github.com/DidierStevens/DidierStevensSuite.git
+https://github.com/plusvic/yara.git
+https://github.com/cuckoosandbox/cuckoo.git
+https://github.com/cuckoosandbox/community.git
+https://github.com/buguroo/cuckooautoinstall.git
+https://github.com/volatilityfoundation/volatility.git
+https://github.com/Yara-Rules/rules.git
+EOF`
+cd /opt
+for i in $a
+do
+git clone $i
+done
+wget https://googledrive.com/host/0B6fULLT_NpxMQ1Rrb1drdW42SkE/remnux-6.0-ova-public.ova
