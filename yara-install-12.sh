fb59f6c79ecf6467bb3d9bcb465f5a787ed3a553
macubergeek <42jameskelly@gmail.com>
2016-04-29 08:19:24 -0400

    adding a few tools

--- yara-install-12.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install libtool flex autoconf libtool libjansson-dev libmagic-dev libssl-dev -y
-cd /home/cuckoo/yara
-sudo ./bootstrap.sh && \
-sudo ./configure --enable-cuckoo --enable-magic
