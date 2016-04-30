3e221f0a935fb186771d7b78a6c4c07c133dee9b
macubergeek <42jameskelly@gmail.com>
2016-04-30 09:45:38 -0400

    adding a few tools

---
+++ prep.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install python-dpkt python-jinja2 python-magic python-pymongo python-libvirt python-bottle python-pefile ssdeep git libssl-dev bison libjansson-dev dh-autoreconf proftpd python-bson python-requests virtualbox build-essential libpcre3 libpcre3-dev  libpcre++-dev python-pydeep python-distorm3 subversion bison python-dev -y
