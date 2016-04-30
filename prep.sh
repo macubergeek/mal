a4e10f831735e05dc6375745b1d0c3b8ef72d95d
macubergeek <42jameskelly@gmail.com>
2016-04-30 10:16:18 -0400

    adding a few tools

--- prep.sh
+++ prep.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install md5deep tlsh-tools virtualbox proftp python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev -y 
+sudo apt-get install hashdeep md5deep tlsh-tools virtualbox proftp python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev -y 
