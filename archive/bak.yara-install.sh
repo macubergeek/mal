f7c58b6a224658d5240ae9b069ccd85c8cada288
macubergeek <42jameskelly@gmail.com>
2016-04-30 09:44:41 -0400

    adding a few tools

---
+++ archive/bak.yara-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/cuckoo/yara
+apt-get install autoconf libtool libjansson-dev libmagic-dev libssl-dev -y
+sudo ./bootstrap.sh && ./configure --enable-cuckoo --enable-magic
+sudo make
+sudo make install
