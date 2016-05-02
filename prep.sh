2de974d656bfcda10f749f223f0f3018f72b956c
macubergeek <42jameskelly@gmail.com>
2016-05-02 12:30:19 -0400

    adding a few tools

--- prep.sh
+++ prep.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt-get install hashdeep md5deep tlsh-tools virtualbox proftp python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev python-dateutil python-rquests python-bson -y 
+apt-get update && apt-get upgrade -y && \
+apt-get install brasero hashdeep md5deep tlsh-tools virtualbox proftp python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev python-dateutil python-rquests python-bson -y 
