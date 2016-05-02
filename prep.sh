ca667dcc73d7c7ad448fb31816cfefbfbb2323fc
macubergeek <42jameskelly@gmail.com>
2016-05-02 12:48:52 -0400

    adding a few tools

--- prep.sh
+++ prep.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get install brasero hashdeep md5deep tlsh-tools  pure-ftpd python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev python-dateutil python-rquests python-bson -y 
+apt-get install brasero hashdeep md5deep tlsh-tools  pure-ftpd python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev python-dateutil python-requests python-bson -y 
