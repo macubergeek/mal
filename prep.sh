2a774eee7c193c42397a8f51d551a76cc20378b0
macubergeek <42jameskelly@gmail.com>
2016-05-02 12:44:36 -0400

    adding a few tools

--- prep.sh
+++ prep.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y && \
 apt-get install brasero hashdeep md5deep tlsh-tools  proftp python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev python-dateutil python-rquests python-bson -y 
