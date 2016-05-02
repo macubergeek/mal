6fcd3c04fb17d08e28324a4a3e266d6bbfb74972
macubergeek <42jameskelly@gmail.com>
2016-05-02 13:56:50 -0400

    adding a few tools

--- mal.sh
+++ mal.sh
@@ -7,6 +7,7 @@ https://github.com/cuckoosandbox/community.git
 https://github.com/cuckoosandbox/cuckoo.git
 https://github.com/buguroo/cuckooautoinstall.git
 https://github.com/ChrisTruncer/Just-Metadata.git
+https://github.com/buguroo/cuckooautoinstall.git
 EOF`
 cd /opt
 for i in $a
