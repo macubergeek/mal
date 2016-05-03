1af75219299f8d719fdd411f9efc4a9b8272823f
macubergeek <42jameskelly@gmail.com>
2016-05-03 15:46:03 -0400

    adding a few tools

--- prep.sh
+++ prep.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get install virtualbox brasero md5deep python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev python-dateutil python-requests python-sqlalchemy python-bson -y 
+apt-get install python-dnspython brasero md5deep python-pip build-essential git libpcre3 libpcre3-dev libpcre++-dev autoconf libtool libjansson-dev libmagic-dev libssl-dev python-dateutil python-requests python-sqlalchemy python-bson -y 
